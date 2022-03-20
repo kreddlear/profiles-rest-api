c9242ffa08359703893ec9a4d151e5e43a849a24
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2022-03-20 15:51:14 -0700

    Added user profile feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,6 +10,7 @@ router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 # don't need a base_name for this one
 # because we have a queryset in our UserProfileViewSet
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
