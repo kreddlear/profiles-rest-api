d2417fad462114a833610c0ef69edc386fc6be01
Katie Redderson-Lear <katie.reddersonlear@zapier.com>
2022-03-20 15:00:27 -0700

    added profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,9 +4,12 @@ from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-
+# don't need a base_name for this one
+# because we have a queryset in our UserProfileViewSet
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
